a7a09ef7cf05722f6cdae226e32c1948550f97a7
Lorena <cejaaimogasta@gmail.com>
2021-10-15 02:38:07 +0200

    ejerciciojava

---
+++ ejercicio.js
@@ -0,0 +1,12 @@
+const nombre = "Lorena";
+
+const apellido = "Naveda";
+
+var edad = 38;
+
+console.log(nombre + " "+ apellido + " " + " tiene " + edad + " años ");
+
+edad = edad + 1; 
+
+
+console.log(nombre + " "+ apellido + " " + " tiene " + edad + " años ");
